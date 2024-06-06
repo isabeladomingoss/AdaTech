908d3f9f04413b18bde6377e8f44873703cbd148
isabeladomingoss <domingosisabela@icloud.com>
2024-06-06 02:22:56 -0300

    reciclagem

--- GIT/git.js
+++ GIT/git.js
@@ -2,4 +2,8 @@
 //git config --global user.email domingosisabela@icloud.com
 
 //git init => iniciar repositório
-//
+//git status => status do arquivo
+//git add => adiciona as mudanças a área de staged changes preparando para o commit
+
+//git diff => ver as modificações
+//git diff --staged => ver diferenças na área de staged
